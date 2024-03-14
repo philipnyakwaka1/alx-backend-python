1ec3bf1c41052c8281f8d5ffc31bddc2e016c3e0
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-03-14 21:39:03 +0300

    create a function that returns the sum of a list of floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+This module defines a type-annotated function sum_list
+which takes a list input_list of floats as argument
+and returns their sum as a float.
+"""
+
+
+def sum_list(input_list: list) -> float:
+    """
+    Finds the average sum of elements of a list of floats
+    Args:
+        input_list (list): List whose sum is to be founds
+    Return:
+        Sum of elements of input_list
+    """
+    return sum(input_list)
