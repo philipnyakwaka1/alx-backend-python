53dc8904de12ad31c92445453638b32873b364ef
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-03-14 21:11:54 +0300

    create function that concatenates to strings

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-import math
 
 """
 This module contains a pe-annotated function
 floor which takes a float n as argument and
 returns the floor of the float.
 """
+import math
 
 
 def floor(n: float) -> int:
