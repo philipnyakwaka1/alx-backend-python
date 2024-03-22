c9165b93f9aa0e37902eeac51b11603539d7f254
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-03-22 15:28:59 +0300

    create a function uses async comprehension to generate a List from a an asynchronous generator

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,11 +3,11 @@
 This module imports async_generator from the previous task and
 then writes a coroutine called async_comprehension that takes no arguments.
 """
-from typing import Generator
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     """
     This coroutine will collect 10 random numbers using an async comprehensing
     over async_generator, then return the 10 random numbers.
