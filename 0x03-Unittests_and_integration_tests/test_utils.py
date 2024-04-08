6613d08b49e2569dcb0b757f686907a4c57da986
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-04-08 19:13:03 +0300

    check raise

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,6 +4,7 @@
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
+from typing import Mapping, Sequence, Any
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -13,8 +14,14 @@ class TestAccessNestedMap(unittest.TestCase):
                            ({"a": {"b": 2}}, ("a",), {"b": 2}),
                            ({"a": {"b": 2}}, ("a", "b"), 2)])
     def test_access_nested_map(self, nested_map, iterable, expected):
-        """Test for access_nested_map function"""
+        """Test for correct output"""
         self.assertEqual(access_nested_map(nested_map, iterable), expected)
+
+    @parameterized.expand([({}, ("a"), KeyError),
+                           ({"a": 1}, ("a", "b"), KeyError)])
+    def test_access_nested_map_exception(self, nested_map, iterable, expected):
+        """Test for KeyError"""
+        self.assertRaises(access_nested_map(nested_map, iterable), expected)
 
 
 if __name__ == '__main__':
