0a8cf4a0547ccac63f3e6d4a0f7de370b82b6dc8
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-04-08 20:03:54 +0300

    test with mock

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,8 +3,10 @@
 
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 from typing import Mapping, Sequence, Any
+from unittest.mock import Mock, patch
+import requests
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -23,6 +25,22 @@ class TestAccessNestedMap(unittest.TestCase):
         """Test for KeyError"""
         with self.assertRaises(expected) as e:
             access_nested_map(nested_map, iterable)
+
+
+class TestGetJson(unittest.TestCase):
+    """Test class for get_json(url) method"""
+
+    @parameterized.expand([("http://example.com", {"payload": True}),
+                           ("http://holberton.io", {"payload": False})])
+    @patch('requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """Test for correct json output"""
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+        result = get_json(test_url)
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
 
 
 if __name__ == '__main__':
