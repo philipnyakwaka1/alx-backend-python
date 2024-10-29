095fbc462aeb6dc46c25bf8c9a409744d3a09c71
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-10-29 15:11:53 +0300

    feat: implement function to concatenate two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This module provides a type-annotated function to concatenate two strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Return a concatenated string
+    Args:
+        str1 (str): The first string to be concatenated
+        str2 (str): The second string to be concatenated
+    Returns:
+        str: Concatenation of str1 and str2
+    """
+
+    return str1 + str2
