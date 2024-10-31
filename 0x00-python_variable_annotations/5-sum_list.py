49590c83255fb2070025c08fd9a2de529275991e
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-10-31 21:39:49 +0300

    feat: implement function to sum elements in a list of floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module containing function to sum all elements of a list
+"""
+from typing import List
+
+def sum_list(my_list: List[float]) -> float:
+    """
+    Return sum of elements in a list of floats
+    Args:
+        my_list (list): List of floats whose elements are to be summed
+    Returns:
+        float: sum of all elements in the argument list
+    """
+
+    return sum(my_list)
